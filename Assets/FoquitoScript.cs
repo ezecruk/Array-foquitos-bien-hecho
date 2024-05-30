ed031dcbf23090e3f0e110bb363ed5af76315a21
ezecruk <164010116+ezecruk@users.noreply.github.com>
2024-05-30 09:06:57 -0300

    Initial commit

---
+++ Assets/FoquitoScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoquitoScript : MonoBehaviour
+{
+    public GameObject[] colors;
+    public int currentLightIndex =-1;
+
+    public void ActivateNextLight()
+    {
+        currentLightIndex++;
+        if (currentLightIndex >= colors.Length)
+        {
+            currentLightIndex = 0;
+        }
+        DeactivateAllLights();
+        colors[currentLightIndex].SetActive(true);
+    }
+
+    public void ActivatePreviousLight()
+    {
+        currentLightIndex--;
+        if (currentLightIndex < 0)
+        {
+            currentLightIndex = colors.Length - 1;
+        }
+        DeactivateAllLights();
+        colors[currentLightIndex].SetActive(true);
+    }
+
+    void DeactivateAllLights()
+    {
+        for ( int i = 0; i < colors.Length; i++)
+        {
+            colors[i].SetActive(false);
+        }
+    }
+
+    public void ActivateRepeating(float t)
+    {
+        CancelInvoke();
+        InvokeRepeating(nameof(ActivateNextLight),0,t);
+    }
+}
